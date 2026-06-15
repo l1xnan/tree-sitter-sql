0b0c55aa98449cd39bdb79ebb9418be699be9401
l1xnan <lixnan@hotmail.com>
2026-06-15 12:06:01 +0800

    fix: switch to WASM-only packaging to avoid native compilation issues on Windows CI

--- bindings/node/index.js
+++ bindings/node/index.js
@@ -1,6 +1,8 @@
+const path = require("path");
 const root = require("path").join(__dirname, "..", "..");
 
-module.exports = require("node-gyp-build")(root);
+const wasmPath = path.join(root, "tree-sitter-sql.wasm");
+module.exports = require("tree-sitter").Language.load(wasmPath);
 
 try {
   module.exports.nodeTypeInfo = require("../../src/node-types.json");
